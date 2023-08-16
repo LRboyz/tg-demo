069100cfaff4b18a1497bb4d5593ae73e86d853a
LRboy <603552916@qq.com>
2023-08-16 14:23:50 +0800

    initial project

---
+++ main.py
@@ -0,0 +1,27 @@
+import telebot
+
+# 在 BotFather 那里获得你自己的 Telegram Bot API Token，并将其粘贴在此处
+TOKEN = '6102219812:AAFQGk0KiEIObTjmAjTISGqODsGBEy5qs9g'
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(func=lambda message: True)
+def calculate(message):
+    # 获取用户发送的消息文本
+    query = message.text
+    print("用户说的消息", query)
+    try:
+        # 使用 eval() 函数来执行字符串形式的数学运算表达式
+        result = eval(query)
+
+        response = "结果为：" + str(result)
+
+    except (SyntaxError, NameError):
+        response = "无法识别您输入的问题，请重新输入"
+
+    bot.reply_to(message, response)
+
+
+if __name__ == '__main__':
+    bot.polling()
